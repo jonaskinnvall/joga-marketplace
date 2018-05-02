bafedcc779703fcd21913210675baf2b31f4648d
Gabe <gabbe631@student.liu.se>
2018-05-02 13:04:09 +0200

    got webpack started

--- webpack.config.js
+++ webpack.config.js
@@ -7,14 +7,14 @@ module.exports = {
   devtool: debug ? "inline-sourcemap" : null,
   entry: "./js/client.js",
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015', 'stage-0'],
-          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
+          plugins: ['transform-class-properties'],
         }
       }
     ]
