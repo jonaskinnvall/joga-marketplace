d325d21ee4650f36d433f1429ffacaa64d84e87e
Jonas Kinnvall <jonaskinnvall@gmail.com>
2018-05-04 09:36:32 +0200

    Fixed linking bug

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import Footer from "./components/Footer"
-import Header from "./components/Header"
+import Footer from "../components/Footer"
+import Header from "../components/Header"
 
 export default class Layout extends React.Component {
 	constructor(){
