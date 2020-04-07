d32e6b37f7cc48936e81b0b89987c81f6331154a
Jonas Kinnvall <jonaskinnvall@gmail.com>
2020-04-07 13:27:05 +0200

    Update App.test to use redux-mock-store, Footer.test to ItemCard.test
    -Copied App.test to ItemCard.test, will implement these tests in another
    branch "tests"

--- src/components/__tests__/ItemCard.test.js
+++ src/components/__tests__/ItemCard.test.js
@@ -1,49 +1,42 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
-// Component to test
-import App from './App';
-
-// Import Provider to wrap compoenet, redux-mock-store and middleware
-import { Provider } from 'react-redux';
 import configureMockStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
 import { logger } from 'redux-logger';
 import thunk from 'redux-thunk';
 
+import ItemCard from '../ItemCard';
+
 // Import auth0 hook to make test work with mock function
-import { useAuth0 } from './Auth/Auth';
+import { useAuth0 } from '../../Auth/Auth';
 // Create dummy user for test
 const user = {
     email: 'johndoe@gmail.com',
     // eslint-disable-next-line babel/camelcase
     email_verified: true,
-    sub: 'google-oauth2|2147627834623744883746'
+    sub: 'google-oauth2|2147627834623744883746',
 };
-jest.mock('./Auth/Auth');
+jest.mock('../../Auth/Auth');
 
-// Create mockStore with middleware
-// Also create initial state
 const mockStore = configureMockStore([thunk, logger]);
 const initialState = { userState: {}, itemState: [] };
 
-describe('First App test', () => {
+describe('ItemCard test', () => {
     let store;
     beforeEach(() => {
         store = mockStore(initialState);
-        // Mock auth0 hook to return logged out state
         useAuth0.mockReturnValue({
             loading: false,
-            isAuthenticated: false,
+            isAuthenticated: true,
             loginWithRedirect: jest.fn(),
-            user: null,
-            logout: jest.fn()
+            user,
+            logout: jest.fn(),
         });
     });
-
-    it('App renders without crashing', () => {
+    it('ItemCard renders', () => {
         shallow(
             <Provider store={store}>
-                <App />
+                <ItemCard />
             </Provider>
         );
     });
