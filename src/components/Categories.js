6452fd1114562b86c6bd4cca3dec1af3b4924740
Jonas Kinnvall <jonaskinnvall@gmail.com>
2020-04-14 23:10:25 +0200

    Update Categories route and move Featured into Home
    -Layout changes to Home and Categories
    -Categories show users favorite category, if chosen, on top and the rest
    under
    -Home shows items with at least one star on top and all items under

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,10 +1,76 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-class Categories extends React.Component {
-    render() {
-        console.log('Categories');
-        return <h1>Categories Page</h1>;
-    }
-}
+import ItemGrid from './ItemGrid';
+const Categories = () => {
+    const userState = useSelector((state) => state.userState);
+    const itemState = useSelector((state) => state.itemState);
+
+    let cats = [...new Set(itemState.map((item) => item.category))];
+    console.log(cats);
+
+    // ? Could be used to place categories with more items over those with lower
+    // let countedCats = itemState.reduce((categories, item) => {
+    //     if (item.category in categories) {
+    //         categories[item.category]++;
+    //     } else {
+    //         categories[item.category] = 1;
+    //     }
+    //     return categories;
+    // }, {});
+
+    // console.log(countedCats);
+
+    return (
+        <>
+            {!Array.isArray(itemState) || !itemState.length ? (
+                <div>Loading...</div>
+            ) : (
+                <>
+                    {!userState.favCat ? (
+                        <>
+                            {cats.map((cat, catID) => {
+                                return (
+                                    <ItemGrid
+                                        key={catID}
+                                        itemsFromState={itemState.filter(
+                                            (item) => item.category === cat
+                                        )}
+                                        title={cat}
+                                        rowLength={4}
+                                    />
+                                );
+                            })}
+                        </>
+                    ) : (
+                        <>
+                            <ItemGrid
+                                itemsFromState={itemState.filter(
+                                    (item) => item.category === userState.favCat
+                                )}
+                                title={userState.favCat}
+                                rowLength={4}
+                            />
+                            {cats
+                                .filter((cat) => cat !== userState.favCat)
+                                .map((cat, catID) => {
+                                    return (
+                                        <ItemGrid
+                                            key={catID}
+                                            itemsFromState={itemState.filter(
+                                                (item) => item.category === cat
+                                            )}
+                                            title={cat}
+                                            rowLength={4}
+                                        />
+                                    );
+                                })}
+                        </>
+                    )}
+                </>
+            )}
+        </>
+    );
+};
 
 export default Categories;
